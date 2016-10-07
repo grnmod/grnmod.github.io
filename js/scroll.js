2b8c64e689889cfa235760ce953d80b87ea76f2b
grnmod <patrickwellgo@gmail.com>
2016-10-07 17:10:26 +0300

    initial commit

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('a[href^="#call"]').click(function(){
         //Сохраняем значение атрибута href в переменной:
         var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top}, 900);
+        $('html, body').animate({scrollTop: $(target).offset().top}, 2500);
         return false;
     });
 
