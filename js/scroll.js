9732e4a554962b1900611d98430ef10de639bfb2
grnmod <patrickwellgo@gmail.com>
2016-11-28 22:37:07 +0300

    initial commit

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('a[href^="#"]').click(function(){
+        //Сохраняем значение атрибута href в переменной:
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 2000);
+        return false;
+    });
+
+});
