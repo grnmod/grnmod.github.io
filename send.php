2dec502b7cd99f207dc12f71aa58671bbb19b15f
grnmod <patrickwellgo@gmail.com>
2016-11-30 18:11:42 +0300

    initial commit

---
+++ send.php
@@ -0,0 +1,21 @@
+<?
+if((isset($_POST['names'])&&$_POST['names']!="")&&(isset($_POST['phone'])&&$_POST['phone']!=""))&&(isset($_POST['console'])&&$_POST['console']!=""))&&(isset($_POST['days'])&&$_POST['days']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'mazgod@gail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заказ приставки'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['names'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>
+                        <p>Приставка: '.$_POST['console'].'</p>
+                        <p>Срок: '.$_POST['days'].'</p>
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
